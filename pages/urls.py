58487587c2d63be43842dc87f60cbc04fc163132
gonzodog22 <gonzodog22@gmail.com>
2026-09-22 12:30:55 -0600

    adding products page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home_page_view, AboutPageView
+from .views import home_page_view, AboutPageView, products_page_view
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
     path("", home_page_view, name="home"),
+    path("products/", products_page_view.as_view(), name="products"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -16,3 +16,14 @@ class AboutPageView(TemplateView):
         context["contact_address"] = "123 Main Street"
         context["phone_number"] = "555-555-5555"
         return context
+class products_page_view(TemplateView):
+    template_name = "products.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_list"] = [
+            {"name": "Bread", "sales_price": 9.99},
+            {"name": "Soup", "sales_price": 3.99},
+            {"name": "Dr.Pepper (12pk)", "sales_price": 5.99},
+            {"name": "Napkins", "sales_price": 1.99},
+        ]
+        return context
